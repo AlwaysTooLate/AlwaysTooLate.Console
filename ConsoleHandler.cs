4e7bfcb7f101149e67525544da8ef1b206f15004
Damian Korczowski <erdroy2@gmail.com>
2019-01-16 13:04:31 +0100

    Update

--- ConsoleHandler.cs
+++ ConsoleHandler.cs
@@ -1,5 +1,7 @@
 ﻿// AlwaysTooLate.Console (c) 2018-2019 Always Too Late.
 
+using AlwaysTooLate.Core;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,55 +9,196 @@ namespace AlwaysTooLate.Console
 {
     public class ConsoleHandler : MonoBehaviour
     {
-        public InputField CommandField;
+        public TMP_InputField CommandField;
+        public ScrollRect ScrollRect;
         public Animation Animation;
+        public Transform Content;
+        public Transform Highlights;
 
-        private void Start()
+        public TMP_FontAsset Font;
+
+        public int FontSize = 14;
+
+        private GameObjectPool _textPool;
+
+        protected void Start()
         {
+            // Setup GameObject pool (+32 elems for highlights)
+            _textPool = new GameObjectPool(ConsoleManager.Instance.MaxMessages + 32, new []
+            {
+                typeof(TextMeshProUGUI), typeof(ContentSizeFitter)
+            });
+
             gameObject.SetActive(false);
+            Highlights.gameObject.SetActive(false);
         }
 
+        protected void OnDestroy()
+        {
+            // Dispose the GameObject pool
+            _textPool.Dispose();
+        }
+        
         public void Open()
         {
             gameObject.SetActive(true);
-
-            CommandField.Select();
-            CommandField.ActivateInputField();
-
+            OnOpenBegin();
+            SetFocus();
             Animation.Play("ConsoleOpen");
         }
 
         public void Close()
         {
+            OnCloseBegin();
             Animation.Play("ConsoleClose");
         }
 
-        public void OnOpenBegin() // Called from animation event
+        public void ClearCommand()
+        {
+            CommandField.text = string.Empty;
+        }
+
+        public void ClearConsole()
+        {
+            while (Content.childCount > 0)
+                _textPool.Release(Content.GetChild(0).gameObject);
+        }
+
+        public void SetFocus()
+        {
+            CommandField.Select();
+            CommandField.ActivateInputField();
+        }
+
+        public void MoveCaretToEnd()
+        {
+            CommandField.MoveTextEnd(false);
+            SetFocus();
+        }
+
+        public void SelectHighlight(int index)
+        {
+            for (var i = 0; i < Highlights.childCount; i++)
+            {
+                var textObject = Highlights.GetChild(i);
+                var textComponent = textObject.GetComponent<TextMeshProUGUI>();
+                textComponent.color = index == i ? Color.yellow : Color.white;
+            }
+        }
+
+        public void SetHighlights(string[] highlights, string markText = "")
+        {
+            while (Highlights.childCount > 0)
+                _textPool.Release(Highlights.GetChild(0).gameObject);
+
+            if (highlights == null || highlights.Length == 0)
+            {
+                Highlights.gameObject.SetActive(false);
+                return;
+            }
+
+            Highlights.gameObject.SetActive(true);
+            
+            foreach (var highlight in highlights)
+            { 
+                // Setup text rect
+                var textObject = _textPool.Acquire();
+
+                // Setup transform
+                var textTransform = (RectTransform)textObject.transform;
+                textTransform.SetParent(Highlights);
+                textTransform.sizeDelta = new Vector2(textTransform.sizeDelta.x, 20);
+               
+                // Setup text
+                var textComponent = textObject.GetComponent<TextMeshProUGUI>();
+                textComponent.text = RichTextExtensions.ColorInnerString(highlight, markText, "green");
+                textComponent.color = Color.white;
+                textComponent.font = Font;
+                textComponent.fontSize = FontSize;
+                textComponent.richText = true; // Enable rich text
+
+                // Setup content size fitter
+                var textFitter = textObject.GetComponent<ContentSizeFitter>();
+                textFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+            }
+
+            var csf = Highlights.GetComponent<ContentSizeFitter>();
+            csf.SetLayoutVertical();
+        }
+
+        public void AddLine(string text)
+        {
+            AddLine(text, Color.white);
+        }
+
+        public void AddLine(string text, Color color)
+        {
+            // Release old children to maintain the desired message count
+            while(Content.childCount >= ConsoleManager.Instance.MaxMessages)
+                _textPool.Release(Content.GetChild(0).gameObject);
+            
+            // Setup text rect
+            var textObject = _textPool.Acquire();
+
+            // Setup transform
+            var textTransform = (RectTransform)textObject.transform;
+            textTransform.SetParent(Content);
+            textTransform.sizeDelta = new Vector2(textTransform.sizeDelta.x, 20);
+
+            // Setup text
+            var textComponent = textObject.GetComponent<TextMeshProUGUI>();
+            textComponent.text = text;
+            textComponent.color = color;
+            textComponent.font = Font;
+            textComponent.fontSize = FontSize;
+            textComponent.richText = true; // Enable rich text
+
+            // Setup content size fitter
+            var textFitter = textObject.GetComponent<ContentSizeFitter>();
+            textFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+            
+            // TODO: Fix scroll to bottom issue.
+            ScrollRect.verticalNormalizedPosition = 0.0f;
+        }
+
+        protected void OnOpenBegin()
         {
             gameObject.SetActive(true);
+            SetHighlights(null);
         }
 
-        public void OnOpenEnd() // Called from animation event
+        protected void OnOpenEnd() // Called from animation event
         {
         }
 
-        public void OnCloseBegin() // Called from animation event
+        protected void OnCloseBegin()
         {
+            ClearCommand();
+            SetHighlights(null);
         }
 
-        public void OnCloseEnd() // Called from animation event
+        protected void OnCloseEnd() // Called from animation event
         {
+            ClearCommand();
             gameObject.SetActive(false);
         }
 
         public void OnTypeCommand(string command)
         {
-
+            ConsoleManager.Instance.OnCommandUpdate(command);
         }
 
         public void OnInputCommand(string command)
         {
+            ConsoleManager.Instance.OnCommandEnter(command);
+        }
 
+        public bool IsEnteringCommand => CommandField.isFocused;
+
+        public string CurrentCommand
+        {
+            get => CommandField.text;
+            set => CommandField.text = value;
         }
     }
 }
--- ConsoleManager.cs
+++ ConsoleManager.cs
@@ -1,5 +1,9 @@
 ﻿// AlwaysTooLate.Console (c) 2018-2019 Always Too Late.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlwaysTooLate.Commands;
 using AlwaysTooLate.Core;
 using UnityEngine;
 
@@ -13,6 +17,14 @@ namespace AlwaysTooLate.Console
     {
         public const string ConsolePrefabName = "AlwaysTooLate.Console";
 
+        public int MaxMessages = 256;
+
+        private readonly List<string> _previousCommands = new List<string>(32);
+        private readonly List<string> _highlights = new List<string>(32);
+
+        [SerializeField]
+        private int _commandCursor;
+
         private GameObject _console;
         private ConsoleHandler _handler;
 
@@ -25,6 +37,14 @@ namespace AlwaysTooLate.Console
             Debug.Assert(consolePrefab, $"Console prefab ({ConsolePrefabName}) not found!");
             _console = Instantiate(consolePrefab);
             _handler = _console.GetComponentInChildren<ConsoleHandler>();
+
+            // Do not destroy the console game object on scene change
+            DontDestroyOnLoad(_console);
+
+            // Listen for logs (strip and add to the console)
+            Application.logMessageReceived += OnLog;
+
+            CommandManager.RegisterCommand("clear", "Clears the console", _handler.ClearConsole);
         }
 
         protected void Update()
@@ -42,6 +62,150 @@ namespace AlwaysTooLate.Console
                     _handler.Close();
                 }
             }
+
+            if (_consoleOpen && _handler.IsEnteringCommand)
+            {
+                // Get previous/next command through Up/Down arrows
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    _commandCursor--;
+                    UpdateHighlights();
+                }
+                else if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    _commandCursor++;
+                    UpdateHighlights();
+                }
+                
+                // Autocomplete
+                if (Input.GetKeyDown(KeyCode.Tab))
+                {
+                    // Try to complete the current command
+                    Autocomplete();
+                }
+            }
         }
+
+        private void UpdateHighlights()
+        {
+            _commandCursor = Mathf.Clamp(_commandCursor, 0, _previousCommands.Count - 1 + _highlights.Count);
+
+            if (IsSelectingHighlight)
+            {
+                _handler.SelectHighlight(_commandCursor - _previousCommands.Count);
+            }
+            else
+            {
+                // Select from history
+                _handler.CurrentCommand = _previousCommands[_commandCursor];
+            }
+        }
+
+        private void OnLog(string condition, string stacktrace, LogType type)
+        {
+            Color color;
+            switch (type)
+            {
+                case LogType.Error:
+                    color = Color.red;
+                    break;
+                case LogType.Assert:
+                    color = Color.red;
+                    break;
+                case LogType.Warning:
+                    color = Color.yellow;
+                    break;
+                case LogType.Log:
+                    color = Color.white;
+                    break;
+                case LogType.Exception:
+                    color = Color.magenta;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+
+            _handler.AddLine(condition, color);
+        }
+
+        public void Autocomplete()
+        {
+            if (IsSelectingHighlight)
+            {
+                var idx = _commandCursor - _previousCommands.Count;
+
+                if (idx < _highlights.Count)
+                {
+                    _handler.CurrentCommand = _highlights[idx];
+                    _handler.MoveCaretToEnd();
+                }
+            }
+            else
+            {
+                if (_handler.CurrentCommand.Length == 0)
+                    return;
+
+                var currentCommand = _handler.CurrentCommand;
+                var commands = CommandManager.GetCommands();
+
+                foreach (var command in commands)
+                {
+                    if (command.Name.StartsWith(currentCommand))
+                    {
+                        _handler.CurrentCommand = command.Name + " ";
+                        break;
+                    }
+                }
+
+                _handler.MoveCaretToEnd();
+            }
+        }
+
+        public void OnCommandUpdate(string command)
+        {
+            var commands = CommandManager.GetCommands();
+
+            _highlights.Clear();
+
+            foreach (var cmd in commands)
+            {
+                if (cmd.Name.Contains(command))
+                {
+                    _highlights.Add(cmd.Name);
+
+                    if (_highlights.Count == 32)
+                        break;
+                }
+            }
+
+            _handler.SetHighlights(_highlights.ToArray(), command);
+        }
+
+        public void OnCommandEnter(string command)
+        {
+            if (command.Length == 0) // Don't even try...
+                return;
+
+            Debug.Log($"> {command}");
+            CommandManager.Execute(command);
+            _handler.ClearCommand();
+            _handler.SetFocus();
+
+            // Add command if not the same
+            if (_previousCommands.Count == 0 || _previousCommands.Last() != command)
+                _previousCommands.Add(command);
+
+            // Set command selector cursor
+            _commandCursor = _previousCommands.Count;
+
+            // Remove commands that are old
+            if(_previousCommands.Count >= 32)
+                _previousCommands.RemoveAt(0);
+
+            _handler.SetHighlights(null);
+            _highlights.Clear();
+        }
+
+        public bool IsSelectingHighlight => _commandCursor >= _previousCommands.Count;
     }
 }
