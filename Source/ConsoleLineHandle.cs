0ac49031dc8beb0b7d8eae953eb3db6d28596d91
Crawcik <crawcik@gmail.com>
2021-08-27 16:20:12 +0200

    Console line highlights

--- Source/ConsoleLineHandle.cs
+++ Source/ConsoleLineHandle.cs
@@ -3,14 +3,32 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace AlwaysTooLate.Console
 {
-    public class ConsoleLineHandle : MonoBehaviour
+    public class ConsoleLineHandle : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         public TMP_Text Text;
         public Image Image;
         public Button Button;
         public RectTransform RectTransform;
+        private bool _isHighlighted;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (_isHighlighted)
+                return;
+            Image.color *= 2f;
+            _isHighlighted = true;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (!_isHighlighted)
+                return;
+            Image.color /= 2f;
+            _isHighlighted = false;
+        }
     }
 }
